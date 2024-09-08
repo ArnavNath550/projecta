8f3d3705e6d4b29421192c498cf551e7baff8042
ArnavNath550 <arnavnath550@gmail.com>
2024-09-09 02:42:48 +1200

    react-spring added, added animation to AnimatedDialog

--- frontend/src/app/packages/ui/animatedDialog/index.tsx
+++ frontend/src/app/packages/ui/animatedDialog/index.tsx
@@ -1,26 +1,47 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
-import { IconX } from '@tabler/icons-react';
+import { useTransition, animated } from 'react-spring';
 
 type Props = {
-    trigger: ReactNode,
-    content: ReactNode
+  trigger: ReactNode,
+  content: ReactNode
 }
 
-function AnimatedDialog(props: Props) {
+function AnimatedDialog({ trigger, content }: Props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const transition = useTransition(isOpen, {
+    from: { opacity: 0, transform: 'scale(0.95)' },
+    enter: { opacity: 1, transform: 'scale(1)' },
+    leave: { opacity: 0, transform: 'scale(0.95)' },
+    config: {
+      tension: 250,
+      friction: 18,
+    },
+  });
+
   return (
-    <Dialog.Root>
-    <Dialog.Trigger asChild>
-      {props.trigger}
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="bg-[#000] opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
-      <Dialog.Content className="data-[state=open]:animate-fadeIn fixed top-[50%] left-[50%] max-h-[85vh] translate-x-[-50%] translate-y-[-50%] rounded-[8px] bg-on-surface-darker border-surface-border border-[1px]  drop-shadow-mdshadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-        {props.content}
-      </Dialog.Content>
-    </Dialog.Portal>
-  </Dialog.Root>
-  )
+    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
+      <Dialog.Trigger asChild>
+        {trigger}
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-[#000] opacity-50 fixed inset-0" />
+        {transition((style, item) =>
+          item ? (
+            <animated.div
+              style={style}
+              className="fixed inset-0 flex items-center justify-center"
+            >
+              <Dialog.Content className="max-h-[85vh] rounded-[8px] bg-on-surface-darker border-surface-border border-[1px] drop-shadow-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+                {content}
+              </Dialog.Content>
+            </animated.div>
+          ) : null
+        )}
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
 }
 
-export default AnimatedDialog
+export default AnimatedDialog;
