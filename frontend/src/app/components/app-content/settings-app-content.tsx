2c4670732adc21aad66567f5dda293db53cade39
ArnavNath550 <arnavnath550@gmail.com>
2024-10-16 10:30:20 +1300

    refactor(settings route added): settings route added

---
+++ frontend/src/app/components/app-content/settings-app-content.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SettingsAppContent() {
+  return (
+    <div className="w-full h-full">
+        <span>Settings page</span>
+    </div>
+  )
+}
+
+export default SettingsAppContent
